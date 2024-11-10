be0c93f71db7a42ea374fd0709eb156e75a79003
dnbragin <dnbragin@outlook.com>
2024-11-10 17:30:13 +0300

    refactor exports

--- index.js
+++
@@ -1,9 +0,0 @@
-export { default as eslintCommon } from './src/eslint/eslint.config.common.js'
-export { default as eslintTypescript } from './src/eslint/eslint.config.typescript.js'
-export { default as eslintReact } from './src/eslint/eslint.config.react.js'
-export { default as eslintNext } from './src/eslint/eslint.config.next.js'
-
-export { default as prettierCommon } from './src/prettier/prettier.config.common.js'
-
-export { default as stylelintCommon } from './src/stylelint/stylelint.config.common.js'
-export { default as stylelintScss } from './src/stylelint/stylelint.config.scss.js'
---
+++ src/index.js
@@ -0,0 +1,9 @@
+export { default as eslintCommon } from './eslint/eslint.config.common.js'
+export { default as eslintTypescript } from './eslint/eslint.config.typescript.js'
+export { default as eslintReact } from './eslint/eslint.config.react.js'
+export { default as eslintNext } from './eslint/eslint.config.next.js'
+
+export { default as prettierCommon } from './prettier/prettier.config.common.js'
+
+export { default as stylelintCommon } from './stylelint/stylelint.config.common.js'
+export { default as stylelintScss } from './stylelint/stylelint.config.scss.js'
