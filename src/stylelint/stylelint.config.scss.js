04a3cc5e7ed9a541f708ddac6296a3380a181881
dnbragin <dnbragin@outlook.com>
2025-03-18 14:22:36 +0300

    update

--- src/stylelint/stylelint.config.scss.js
+++ src/stylelint/stylelint.config.scss.js
@@ -8,10 +8,10 @@ const propertiesOrder = propertyGroups.map((properties) => ({
 
 export default {
   extends: [
-    'stylelint-config-recommended',
+    'stylelint-config-standard',
     'stylelint-config-standard-scss',
-    'stylelint-config-sass-guidelines',
-    // 'stylelint-config-tailwindcss',
+    // 'stylelint-config-sass-guidelines',
+    // 'stylelint-config-tailwindcss/scss',
   ],
   plugins: ['stylelint-order', 'stylelint-prettier'],
   rules: {
