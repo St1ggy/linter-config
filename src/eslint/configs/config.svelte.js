2a4e2484057a312a2067cc3c62d83b09178069de
dnbragin <dnbragin@outlook.com>
2025-05-07 18:59:51 +0300

    fix svelte

--- src/eslint/configs/config.svelte.js
+++ src/eslint/configs/config.svelte.js
@@ -4,6 +4,8 @@ import { parser } from 'typescript-eslint'
 
 import { files } from './constants.js'
 
+const extensions = ['.js', '.ts', '.svelte']
+
 export default defineConfig([
   sveltePlugin.configs['flat/recommended'],
   {
@@ -17,9 +19,20 @@ export default defineConfig([
         parser,
       },
     },
+    'import/parsers': {
+      'svelte-eslint-parser': ['.svelte'],
+      espree: extensions,
+    },
     settings: {
       'import/resolver': {
-        svelte: true,
+        'eslint-import-resolver-custom-alias': {
+          alias: {
+            $lib: 'src/lib',
+            $app: 'node_modules/@sveltejs/kit/src/runtime/app',
+            '@sveltejs/kit': 'node_modules/@sveltejs/kit/src/exports/index.js',
+          },
+          extensions,
+        },
       },
     },
   },
