91fe61d407768c91ed7ae6d1b2d02aca26fee844
dnbragin <dnbragin@outlook.com>
2025-06-16 17:52:48 +0300

    add common config

---
+++ scripts/init-common.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+declare -A postfix_map=(
+  [eslint]="common"
+  [stylelint]="scss"
+  [prettier]="common"
+)
+
+for l in eslint stylelint prettier; do
+  echo "creating $l.config.js with ${l}-${postfix_map[$l]}"
+  echo "export { default } from '@st1ggy/linter-config/${l}-${postfix_map[$l]}';" > $l.config.js;
+done
