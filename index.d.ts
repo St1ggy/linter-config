a4477ccdaf2ba4da5f312e5736aae177b72ca0f2
dnbragin <dnbragin@outlook.com>
2025-11-02 17:56:42 +0300

    update types

--- index.d.ts
+++ index.d.ts
@@ -1,49 +1,45 @@
-import type { Linter } from 'eslint'
+import { type ConfigArray } from 'eslint'
+import { type Config as PrettierConfig } from 'prettier'
+import { type Config as StylelintConfig } from 'stylelint'
 
 declare module '@st1ggy/linter-config/eslint-next' {
-  const config: Linter.Config[]
+  const config: ConfigArray
 
   export default config
 }
 
 declare module '@st1ggy/linter-config/eslint-react' {
-  const config: Linter.Config[]
+  const config: ConfigArray
 
   export default config
 }
 
 declare module '@st1ggy/linter-config/eslint-svelte' {
-  const config: Linter.Config[]
+  const config: ConfigArray
 
   export default config
 }
 
 declare module '@st1ggy/linter-config/eslint-common' {
-  const config: Linter.Config[]
+  const config: ConfigArray
 
   export default config
 }
 
 declare module '@st1ggy/linter-config/prettier-common' {
-  import type { Config } from 'prettier'
-
-  const config: Config
+  const config: PrettierConfig
 
   export default config
 }
 
 declare module '@st1ggy/linter-config/prettier-svelte' {
-  import type { Config } from 'prettier'
-
-  const config: Config
+  const config: PrettierConfig
 
   export default config
 }
 
 declare module '@st1ggy/linter-config/stylelint-scss' {
-  import type { Config } from 'stylelint'
-
-  const config: Config
+  const config: StylelintConfig
 
   export default config
 }
